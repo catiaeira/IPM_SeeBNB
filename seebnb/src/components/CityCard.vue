5427e4e58055cb6f85ca769a209abd56228bbc57
MarcoFerreira05 <a106857@alunos.uminho.pt>
2025-12-23 14:52:46 +0000

    Add CityCard and MostViewedCities components

---
+++ seebnb/src/components/CityCard.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="city-card">
+        <img class="city-image"
+        :src="image"
+        :alt="`Imagem de ${cityname}`"
+        />
+        <div class="overlay">
+            <h2 class="city-name">{{ cityname }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CityCard",
+    props: {
+    cityname: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.city-card {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    border-radius: 20px;
+    overflow: hidden;
+    cursor: pointer;
+    transition: transform 0.3s ease;
+}
+
+.city-card:hover {
+    transform: translateY(-5px);
+}
+
+.city-image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    transition: transform 0.4s ease;
+    z-index: 1;
+}
+
+.city-card:hover .city-image {
+    transform: scale(1.05);
+}
+
+.overlay {
+    position: absolute;
+    inset: 0;
+    display: flex;
+    align-items: flex-end;
+    padding: 16px;
+    z-index: 2;
+}
+
+.city-name {
+    color: var(--image_text);
+    font-size: 2rem;
+    font-weight: 600;
+    margin: 0;
+    text-shadow:
+        0 0 2px var(--text),
+        0 0 4px var(--text),
+        0 0 6px var(--text),
+        0 0 8px var(--text);
+}
+</style>
---
+++ seebnb/src/components/MostVisitedCities.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container">
+        <h2 class="text">Cidades mais visitadas</h2>
+        <div class="divider"></div>
+        <div class="city-table">
+            <CityCard
+                v-for="city in cities"
+                :key="city.id"
+                :cityname="city.cityname"
+                :image="city.image"
+            />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import CityCard from "@/components/CityCard.vue"
+
+const cities = [
+    { id: 1, cityname: "Milão", image: "https://images.unsplash.com/photo-1610016302534-6f67f1c968d8" },
+    { id: 2, cityname: "Manchester", image: "https://images.unsplash.com/photo-1619284518317-85b1ab8c7723" },
+    { id: 3, cityname: "Sevilha", image: "https://images.unsplash.com/photo-1688404808661-92f72f2ea258" },
+    { id: 4, cityname: "Roma", image: "https://plus.unsplash.com/premium_photo-1675975706513-9daba0ec12a8" },
+    { id: 5, cityname: "Tokyo", image: "https://images.unsplash.com/photo-1557409518-691ebcd96038" },
+    { id: 6, cityname: "Sydney", image: "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9" },
+    { id: 7, cityname: "Londres", image: "https://images.unsplash.com/photo-1500380804539-4e1e8c1e7118" },
+    { id: 8, cityname: "Paris", image: "https://images.unsplash.com/photo-1431274172761-fca41d930114" },
+]
+</script>
+
+<style scoped>
+.container {
+    border: 5px solid var(--secondary);
+    border-radius: 20px;
+    padding: 20px;
+}
+
+.divider {
+    height: 7px;
+    width: 100%;
+    background-color: var(--accent);
+    border-radius: 5px;
+    margin: 0 0 20px 0;
+}
+.city-table {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 16px;
+}
+
+.text {
+    font-size: 2.2rem;
+    font-weight: 600;
+    color: var(--text);
+}
+</style>
