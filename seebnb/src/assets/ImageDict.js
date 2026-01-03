50c071e9b699524c62a9bdea5907d28fe4e5b7d9
catiaeira <catia.eira71@gmail.com>
2026-01-03 12:05:28 +0000

    font and images update

--- seebnb/src/assets/ImageDict.js
+++ seebnb/src/assets/ImageDict.js
@@ -1,13 +1,19 @@
-import Moon from '@/assets/moon.png'
-import Sun from '@/assets/sun.png'
-import Logo from '@/assets/seebnblogo.png'
+import Moon from '@/assets/NavBarIcons/moon.png'
+import Sun from '@/assets/NavBarIcons/sun.png'
+import Logo from '@/assets/NavBarIcons/seebnblogo.png'
 import Arrow from '@/assets/iconmonstr-arrow.png'
+import ShareDark from '@/assets/NavBarIcons/share-dark.png'
+import ShareLight from '@/assets/NavBarIcons/share-light.png'
 
 export default {
     logo: Logo,
-    moon: Moon,
-    sun: Sun,
+    moon: Moon, // <a href="https://www.flaticon.com/free-icons/night-mode" title="night mode icons">Night mode icons created by Esma Başoğlu - Flaticon</a>
+    sun: Sun, // <a href="https://www.flaticon.com/authors/freepik" title="Freepik"> Freepik </a>
     arrow: Arrow,
+
+    // <a href="https://www.flaticon.com/free-icons/share" title="share icons">Share icons created by edt.im - Flaticon</a>
+    shareDark: ShareDark,
+    shareLight: ShareLight,
 
     cityscape:  "https://images.unsplash.com/photo-1708474660686-7a00a0ffe4ac",
 
@@ -18,7 +24,7 @@ export default {
     paris:      "https://images.unsplash.com/photo-1431274172761-fca41d930114",
     porto:      "https://images.unsplash.com/photo-1555881400-74d7acaacd8b",
     rome:       "https://images.unsplash.com/photo-1526161202719-67a7615a3979",
-    sevilla:    "https://images.unsplash.com/photo-1688404808661-92f72f2ea258",
+    seville:    "https://images.unsplash.com/photo-1688404808661-92f72f2ea258",
     sydney:     "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9",
     tokyo:      "https://images.unsplash.com/photo-1557409518-691ebcd96038"
 }
