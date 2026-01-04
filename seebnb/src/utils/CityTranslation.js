61d723146eac1df62975c114ce70ab485934ae98
cosqf <filipacosquete@gmail.com>
2026-01-04 13:19:13 +0000

    translate city name to pt

--- seebnb/src/utils/CityTranslation.js
+++ seebnb/src/utils/CityTranslation.js
@@ -9,3 +9,18 @@ export function normalizeCityName(original) {
   let normLocation = original.trim().toLowerCase();
   return CityTranslation[normLocation]  || normLocation;
 }
+
+function capitalize(str) {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function translateToPT(englishName) {
+  const target = englishName.trim().toLowerCase();
+  
+  const ptName = Object.keys(CityTranslation).find(
+    (key) => CityTranslation[key] === target
+  );
+
+  return capitalize(ptName || target);
+}
